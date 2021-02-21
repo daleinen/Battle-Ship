159cdd4b50790a4cd05f9809059e8f0ccf6e3634
David Leinen <daleinen@hotmail.com>
2021-02-21 17:06:05 -0600

    21FEB21

--- BattleShip/BattleShip.py
+++ BattleShip/BattleShip.py
@@ -1,20 +1,29 @@
-#Battleship 1.0
-#dleinen 09SEP15
-#Python2
+"""
+***-------------------------------------------------------------------|
+Title:      BattleShip
+Version:    1.0.0
+Date:       21FEB2021
+Author:     David Leinen (daleinen@hotmail.com)
+
+Program randomly evovles text to match that of user entry
+Run from any command line with the Python 3
+
+***-------------------------------------------------------------------|
+"""
 
 from random import randint 
 
 #************************* functions *************************
-#function to print board formated correctly
-def print_board(board):
-    for row in board:
+#function to print board formatted correctly
+def print_board(b):
+    for row in b:
         print(" ".join(row))
 
 #function getting random numbers for battleship
-def random_row(board):
-    return randint(0, len(board) - 1)
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
+def random_row(b):
+    return randint(0, len(b) - 1)
+def random_col(b):
+    return randint(0, len(b[0]) - 1)
 
 #************************* end of functions *************************
 
@@ -28,17 +37,19 @@ ship_row = random_row(board)
 ship_col = random_col(board)
 
 #printing welcome message
-print("\nLet's play Battleship!")
+print("\nWelcome to BattleShip v1.0")
+print("Let's play Battleship!")
 print("You have 5 guesses\n")
 print_board(board)
+print()
 
 #giving the player 5 turns
 for turn in range(0,5):
     
     #getting player input for row and column
-    guess_row = int(raw_input("Guess Row: "))
-    guess_col = int(raw_input("Guess Col: "))
-    #subtracting one to change collumn/row 0 to 1
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
+    #subtracting one to change column/row 0 to 1
     guess_row = guess_row - 1
     guess_col = guess_col - 1
     
@@ -46,7 +57,7 @@ for turn in range(0,5):
     print("\nTurn:"), turn + 1
     
     #correct guess
-    if (guess_row == ship_row and guess_col == ship_col):
+    if guess_row == ship_row and guess_col == ship_col:
         board[ship_row][ship_col] = "B"
         print_board(board)
         print("\nCongratulations! You sunk my battleship!")
@@ -57,7 +68,7 @@ for turn in range(0,5):
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
             print("Oops, that's not even in the ocean.")
         #same guess    
-        elif(board[guess_row][guess_col] == "X"):
+        elif board[guess_row][guess_col] == "X":
             print("You guessed that one already.")
         #place a guess for miss
         else:
